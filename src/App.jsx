6f8d3287fa0b2cf8d78cfa5750a3d11ec755076e
Amdadul-HQ <rimonamdadul301@gmail.com>
2024-08-31 15:45:59 +0600

    adding tip tap

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { EditorProvider, FloatingMenu, BubbleMenu } from '@tiptap/react'
+import StarterKit from '@tiptap/starter-kit'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const extensions = [StarterKit]
+
+const content = '<p>Hello World!</p>'
+
 
   return (
     <>
-      
+      <EditorProvider extensions={extensions} content={content}>
+      <FloatingMenu editor={null}>This is the floating menu</FloatingMenu>
+      <BubbleMenu editor={null}>This is the bubble menu</BubbleMenu>
+    </EditorProvider>
     </>
   )
 }
